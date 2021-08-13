2741498edb2553b07e5aba64365bd3a976a696ab
long.chau.ext <long.chau.ext@personio.de>
2021-08-14 00:52:05 +0700

    Rewrite plugin to lua.

--- bin/learn_import.sh
+++ bin/learn_import.sh
@@ -3,4 +3,4 @@ sample=${SAMPLE_PATH:="$HOME/Projects/personio"}
 
 rg -U -I -t kotlin import $sample > ~/.import.lib
 no_dup_data=$(awk '!a[$0]++' ~/.import.lib)
-echo $no_dup_data > ~/.import.lib
+echo $no_dup_data | sed 's/import //g' > ~/.import.lib

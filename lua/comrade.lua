ec2f1bd732e8ad325273b6287020d231dc505fd5
long.chau.ext <long.chau.ext@personio.de>
2021-08-15 01:29:40 +0700

    Fix bug, currently only import the first candidate.

--- lua/comrade.lua
+++ lua/comrade.lua
@@ -4,17 +4,21 @@ local M = {}
 
 function M.import()
   local insights = vim.fn['comrade#bvar#get'](vim.fn.bufnr(''), 'insight_map')
+  local imports = {}
 
   for _, insight in pairs(insights) do
     for _, item in ipairs(insight) do
       for key, value in pairs(item) do
         if key == 'desc' and string.find(value, 'Unresolved reference: ') ~= nil then
           local import_candidate = string.gsub(value, "Unresolved reference: ", "")
-          importkt.import(import_candidate)
+          table.insert(imports, import_candidate)
         end
       end
     end
   end
+
+	-- Currently, only run the first candidate
+		importkt.import(imports[1])
 end
 
 return M
